b75a983b0899388807da398b3df6ef2c0c299730
DersimCloudHT24 <dersim.abbas@iths.se>
2024-08-28 14:41:31 +0200

    nr 6

---
+++ if-else-nr6/if-else-nr6/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("please type any number, can be positive or negative or even zero.");
+
+        float UserNum = float.Parse(Console.ReadLine());
+
+        if (UserNum < 0)
+        {
+
+
+            Console.WriteLine($"your number {UserNum} is negative");
+
+
+        }
+        else if (UserNum > 0)
+        {
+            Console.WriteLine($"your number {UserNum} is Positive");
+        }
+        else
+        {
+            Console.WriteLine("your number is 0");
+        }
+
+        
+    }
+} 
