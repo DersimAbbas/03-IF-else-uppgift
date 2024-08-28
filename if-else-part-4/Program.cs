4eabbb355e0a86b93281f4912fde27fd900a52a2
DersimCloudHT24 <dersim.abbas@iths.se>
2024-08-28 11:17:42 +0200

    part 4

---
+++ if-else-part-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Metadata;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please type in a year");
+        
+        int Usernum = Int32.Parse(Console.ReadLine());
+
+        if (Usernum % 4 == 0)
+        {
+            Console.WriteLine($"{Usernum} is a leap year.");
+
+        }
+        else
+        {
+            Console.WriteLine($"{Usernum} is not a leap year");
+        }
+        
+
+
+    }
+}
