29489ad59e40e027b775550838b51cf165761f08
DersimCloudHT24 <dersim.abbas@iths.se>
2024-08-28 19:39:17 +0200

    uppgift 9

---
+++ IF-else-part9/IF-else-part9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Figgle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string password = "cloud24";
+
+        Console.WriteLine("Please type in your password:");
+
+        string userInput = Console.ReadLine();
+
+        string accessGranted = "Access Granted";
+        string accessDenied = "Access Denied";
+
+        if (userInput == password)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(FiggleFonts.Standard.Render(accessGranted));
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(FiggleFonts.Standard.Render(accessDenied));
+        }
+
+        // Reset the console color to default
+        Console.ResetColor();
+    }
+}
